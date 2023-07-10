79b9fbc086d0bafdb9882549be3f2da1e1241232
saipranayteja <pranayteja.ch@algonox.com>
2023-07-11 04:02:24 +0530

    azsxdfsxdcf

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,3 @@
 source /params/params.txt
-java -XX:MinRAMPercentage=70 -XX:MaxRAMPercentage=90 -jar /app/shipping.jar
+java -jar /app/shipping.jar
+# java -XX:MinRAMPercentage=70 -XX:MaxRAMPercentage=90 -jar /app/shipping.jar
